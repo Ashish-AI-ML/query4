b82056109d3ee5924151734e992e3a13d5c2c7c0
Ashish chaturvedi <ashishchaturvedi758@gmail.com>
2025-03-28 15:44:00 +0530

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,18 @@
+use [Test Table]
+
+INSERT INTO Testtable
+
+(PatientID ,PatientName ,PatientState ,Gender ,Visits ,Charges)
+
+VALUES ('12345','John','AL','M','3','200')
+,('12346','Jane','AK','F','1','400')
+,('12347','Alex','AZ','F','6','900')
+,('12348','Bob','CA','M','7','8000')
+,('12349','Josh','CO','M','12','19000')
+,('12350','Stephanie','FL','F','18','25000')
+,('12351','Amber','GA','F','4','400')
+,('12352','Brittany','GA','F','6','4000')
+,('12353','Bill','UT','M','8','5000')
+,('12354','Nate','WY','M','22','28000')
+
+select * from Testtable
